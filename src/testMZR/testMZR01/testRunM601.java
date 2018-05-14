b8e5a6f946608d850d1ef834dc73203687e9ac6e
kyaccp <kyaccp@ky8jf019>
2018-05-14 11:59:59 +0800

    1:第一次提交Git

---
+++ src/testMZR/testMZR01/testRunM601.java
@@ -0,0 +1,10 @@
+package testMZR.testMZR01;
+
+public class testRunM601 {
+
+	public static void main(String[] args) {
+		System.out.println("哈哈哈......我的第一次！11111111");
+
+	}
+
+}
---
+++ src/testMZR/testMZR02/testRunM604.java
@@ -0,0 +1,10 @@
+package testMZR.testMZR02;
+
+public class testRunM604 {
+
+	public static void main(String[] args) {
+		System.out.println("哈哈哈......我的第一次！44444444");
+
+	}
+
+}
